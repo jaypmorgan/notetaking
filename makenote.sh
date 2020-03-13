2194cf0872103d865681286b4b40300fee0136d6
Jay Morgan <jaymiles17@gmail.com>
2020-03-13 18:57:43 +0100

    updated makenote script

--- makenote.sh
+++ makenote.sh
@@ -6,7 +6,7 @@ TITLE=$(sed 's|\s|-|g' <<< $TITLE)
 FILENAME="$(date --iso-8601='date')-$TITLE.tex"
 touch "$FILENAME"
 echo """\\documentclass[a4paper]{article}
-\\usepackage{fancyNotes}
+\\usepackage{includes/fancyNotes}
 
 \\title{$TITLE}
 \\begin{document}
