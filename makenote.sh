ac9e5020d96ddcb0d710a5642838413683c257f8
Jay Morgan <jaypmorgan@users.noreply.github.com>
2020-03-13 17:52:17 +0000

    added basic note taking files

---
+++ makenote.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+printf 'Title: '
+read TITLE
+TITLE=$(sed 's|\s|-|g' <<< $TITLE)
+FILENAME="$(date --iso-8601='date')-$TITLE.tex"
+touch "$FILENAME"
+echo """\\documentclass[a4paper]{article}
+\\usepackage{fancyNotes}
+
+\\title{$TITLE}
+\\begin{document}
+
+
+\\end{document}""" >> "$FILENAME"
+vim "$FILENAME"
